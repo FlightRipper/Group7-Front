cf9373a2e42db05ccca20a289d7997484580551a
anitta-sleiman <sleiman3198@gmail.com>
2023-11-05 10:34:27 +0200

    edited components: header and navbar

--- front/src/components/Navbar/navbar.jsx
+++ front/src/components/Navbar/navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
           <Link to='/'>Home</Link>
         </li>
         <li className="nav-item">
-          <div className="shop-link" onClick={toggleDropdown}>
-            Shop <img src={arrow} alt="arrow" className="arrow-logo" />
-          </div>
+          <span className="shop-text">Shop</span> 
+          <img src={arrow} alt="arrow" className="arrow-logo" onClick={toggleDropdown} />
+      
           {isDropdownVisible && (
             <ul className="dropdown-menu">
               <li className="menu-item">
@@ -55,3 +55,60 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+// import React, { useState } from "react";
+// import arrow from "../../Assets/Logo/arrow.svg";
+// import TechSpot from "../../Assets/Images/TechSpot-logo.png";
+// import "./navbar.css";
+// import { NavLink } from 'react-router-dom';
+
+// const Navbar = () => {
+//   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+//   const toggleDropdown = () => {
+//     setIsDropdownVisible(!isDropdownVisible);
+//   };
+
+//   return (
+//     <nav className="App-navbar">
+//       <img className="techspot-logo" src={TechSpot} alt="website-logo" />
+//       <ul className="nav-list">
+//         <li className="nav-item">
+//           <NavLink to='/' activeClassName="active-link">Home</NavLink>
+//         </li>
+//         <li className="nav-item">
+//           <div className="shop-link" onClick={toggleDropdown}>
+//             Shop <img src={arrow} alt="arrow" className="arrow-logo" />
+//           </div>
+//           {isDropdownVisible && (
+//             <ul className="dropdown-menu">
+//               <li className="menu-item">
+//                 <a href="/Phones">Phones</a>
+//               </li>
+//               <li className="menu-item">
+//                 <a href="/Laptops">Laptops</a>
+//               </li>
+//               <li className="menu-item">
+//                 <a href="/Phone Accessories">Phone Accessories</a>
+//               </li>
+//               <li className="menu-item">
+//                 <a href="/Laptop Accessories">Laptop Accessories</a>
+//               </li>
+//             </ul>
+//           )}
+//         </li>
+//         <li className="nav-item">
+//           <a href="/blogs">Blog</a>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink to="/contact" activeClassName="active-link">Contact Us</NavLink>
+//         </li>
+//         <li className="nav-item">
+//           <NavLink to="/about" activeClassName="active-link">About Us</NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
