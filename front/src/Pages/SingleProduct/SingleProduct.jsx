a052e5de873428efbffcc9d541c28f2752cf58d3
anitta-sleiman <sleiman3198@gmail.com>
2023-11-07 10:46:26 +0200

    SingleProduct with fetch

--- front/src/Pages/SingleProduct/SingleProduct.jsx
+++ front/src/Pages/SingleProduct/SingleProduct.jsx
@@ -1,9 +1,143 @@
+// backup
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import "./SingleProduct.css";
+// import leftArrow from "../../Assets/Logo/leftarrow.svg";
+// import rightArrow from "../../Assets/Logo/rightarrow.svg";
+// import Slider from "react-slick";
+// import Header from "../../components/Header/header.jsx";
+// import Navbar from "../../components/Navbar/navbar.jsx";
+// import Footer from "../../components/Footer/footer.jsx";
+// import GoBack from "../../components/GoBack/GoBack.js";
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+
+// export default function SingleProduct() {
+//   const { productId } = useParams();
+//   const [Product, setProduct] = useState(null);
+
+//   useEffect(() => {
+//     // Replace '123' with the actual product ID you want to fetch
+//     // fetch('http://localhost:4000/api/products/653fb27967a73e001776485d')
+//     // fetch("http://localhost:4000/api/products/65493daffbda558f8efc520b")
+//     fetch(`http://localhost:4000/api/products/${productId}`)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+//         return response.json();
+//       })
+//       .then((data) => setProduct(data))
+//       .catch((error) => {
+//         console.error("Error:", error);
+//         setProduct(null);
+//       });
+//   }, [productId]);
+
+//   const settings = {
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 5000,
+//     prevArrow: <img className="arrow1" src={leftArrow} alt="Left Arrow" />,
+//     nextArrow: <img className="arrow1" src={rightArrow} alt="Right Arrow" />,
+//     style: {
+//       width: "20vw",
+//       height: "70vh",
+//       borderRadius: "30px",
+//       boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+//       background:
+//         "linear-gradient(154deg, #105AB2 38%, rgba(255, 255, 255, 0.00) 128.25%)",
+//     },
+//   };
+
+//   return (
+//     <>
+//       <Header />
+//       <Navbar />
+//       <div className="SingleProduct">
+//         <div className="SingleProduct_name">
+//           {Product !== null && (
+//             <span className="product_name">{[Product.description]}</span>
+//           )}
+//           <GoBack />
+//         </div>
+//         <div className="carousel_container">
+//           {Product !== null && (
+//             <Slider {...settings}>
+//               {Product.imagePath.map((image, index) => (
+//                 <div key={index}>
+//                   <img
+//                     className="cimage"
+//                     src={"http://localhost:4000/" + image}
+//                     alt={`${index}`}
+//                   />
+//                 </div>
+//               ))}
+//             </Slider>
+//           )}
+//         </div>
+//         <div className="desc">
+//           {Product !== null && (
+//             <h3>
+//               $ <span>{Product.price}</span>
+//             </h3>
+//           )}
+//           <form>
+//             <ul>
+//               {Product !== null && <li>Name: {Product.name}</li>}
+//               {Product !== null && <li>Description: {Product.description}</li>}
+
+//               {/*Laptop/Phone attributes */}
+//               {Product !== null && Product.attributes.operatingSystem && (
+//                 <li>
+//                   OperatingSystem (OS): {Product.attributes.operatingSystem}
+//                 </li>
+//               )}
+//               {Product !== null && Product.attributes.camera && (
+//                 <li>Camera: {Product.attributes.camera}</li>
+//               )}
+//               {Product !== null && Product.attributes.display && (
+//                 <li>Display: {Product.attributes.display}</li>
+//               )}
+//               {Product !== null && Product.attributes.battery && (
+//                 <li>Battery: {Product.attributes.battery}</li>
+//               )}
+//               {Product !== null && Product.attributes.ram && (
+//                 <li>RAM: {Product.attributes.ram}</li>
+//               )}
+//               {Product !== null && Product.attributes.cpu && (
+//                 <li>CPU: {Product.attributes.cpu}</li>
+//               )}
+//               {Product !== null && Product.attributes.storage && (
+//                 <li>Storage: {Product.attributes.storage}</li>
+//               )}
+//               {/*Accessories attributes */}
+//               {Product !== null && Product.attributes.accessoriesBrand && (
+//                 <li>Brand: {Product.attributes.accessoriesBrand}</li>
+//               )}
+//               {Product !== null && Product.attributes.accessoriesColor && (
+//                 <li>Color: {Product.attributes.accessoriesColor}</li>
+//               )}
+//               {Product !== null && Product.attributes.accessoriesType && (
+//                 <li>Type: {Product.attributes.accessoriesType}</li>
+//               )}
+//             </ul>
+//           </form>
+//         </div>
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// fetch
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./SingleProduct.css";
-import mouse from "../../Assets/Images/mouse-removebg-preview.png";
-import phone1 from "../../Assets/Images/Screenshot_from_2023-10-18_16-11-21-removebg-preview.png";
-import phone from "../../Assets/Images/Screenshot_from_2023-10-18_17-00-19-removebg-preview.png";
 import leftArrow from "../../Assets/Logo/leftarrow.svg";
 import rightArrow from "../../Assets/Logo/rightarrow.svg";
 import Slider from "react-slick";
@@ -11,7 +145,31 @@ import Header from "../../components/Header/header.jsx";
 import Navbar from "../../components/Navbar/navbar.jsx";
 import Footer from "../../components/Footer/footer.jsx";
 import GoBack from "../../components/GoBack/GoBack.js";
-export default function SimpleSlider() {
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function SingleProduct() {
+  const { productId } = useParams();
+  const [Product, setProduct] = useState(null);
+
+  useEffect(() => {
+    // Replace '123' with the actual product ID you want to fetch
+    // fetch('http://localhost:4000/api/products/653fb27967a73e001776485d')
+    // fetch("http://localhost:4000/api/products/65493daffbda558f8efc520b")
+    fetch(`http://localhost:4000/api/products/${productId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setProduct(data))
+      .catch((error) => {
+        console.error("Error:", error);
+        setProduct(null);
+      });
+  }, [productId]);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -26,13 +184,9 @@ export default function SimpleSlider() {
       width: "20vw",
       height: "70vh",
       borderRadius: "30px",
-      // display: "flex",
-      // alignItems: "center",
-      // justifyContent: "center",
-      // alignContent: "center",
       boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-      background: "linear-gradient(154deg, #105AB2 38%, rgba(255, 255, 255, 0.00) 128.25%)"
-
+      background:
+        "linear-gradient(154deg, #105AB2 38%, rgba(255, 255, 255, 0.00) 128.25%)",
     },
   };
 
@@ -42,37 +196,109 @@ export default function SimpleSlider() {
       <Navbar />
       <div className="SingleProduct">
         <div className="SingleProduct_name">
-          <span className="product_name">Galaxy Note 10+</span>
-          <GoBack/>
+          {Product !== null && (
+            <span className="product_name">{[Product.description]}</span>
+          )}
+          <GoBack />
         </div>
         <div className="carousel_container">
-          <Slider {...settings}>
-            <div>
-              <img className="cimage" height="500" src={mouse} alt="mouse" />
-            </div>
-            <div>
-              <img className="cimage" src={phone1} alt="logo" />
-            </div>
-            <div>
-              <img className="cimage" src={phone} alt="phone" />
-            </div>
-          </Slider>
+          {Product !== null && (
+            <Slider {...settings}>
+              {Product.imagePath.map((image, index) => (
+                <div key={index}>
+                  <img
+                    className="cimage"
+                    src={"http://localhost:4000/" + image}
+                    alt={`${index}`}
+                  />
+                </div>
+              ))}
+            </Slider>
+          )}
         </div>
         <div className="desc">
-          <h3>
-            $ <span>Price</span>
-          </h3>
+          {Product !== null && (
+            <h3>
+              $ <span>{Product.price}</span>
+            </h3>
+          )}
           <form>
             <ul>
-              <li>Name</li>
-              <li>Description</li>
-              <li>OperatingSystem (OS)</li>
-              <li>Camera</li>
-              <li>Display</li>
-              <li>Battery</li>
-              <li>RAM</li>
-              <li>CPU</li>
-              <li>Storage</li>
+              {Product !== null && (
+                <li>
+                  <span className="label">Name: </span>
+                  {Product.name}
+                </li>
+              )}
+              {Product !== null && (
+                <li>
+                  <span className="label">Description: </span>{" "}
+                  {Product.description}
+                </li>
+              )}
+
+              {/*Laptop/Phone attributes */}
+              {Product !== null && Product.attributes.operatingSystem && (
+                <li>
+                  <span className="label">Operating System (OS): </span>
+                  {Product.attributes.operatingSystem}
+                </li>
+              )}
+              {Product !== null && Product.attributes.camera && (
+                <li>
+                  <span className="label">Camera: </span>
+                  {Product.attributes.camera}
+                </li>
+              )}
+              {Product !== null && Product.attributes.display && (
+                <li>
+                  <span className="label">Display: </span>{" "}
+                  {Product.attributes.display}
+                </li>
+              )}
+              {Product !== null && Product.attributes.battery && (
+                <li>
+                  <span className="label">Battery: </span>
+                  {Product.attributes.battery}
+                </li>
+              )}
+              {Product !== null && Product.attributes.ram && (
+                <li>
+                  <span className="label">RAM: </span>
+                  {Product.attributes.ram}
+                </li>
+              )}
+              {Product !== null && Product.attributes.cpu && (
+                <li>
+                  <span className="label">CPU: </span>
+                  {Product.attributes.cpu}
+                </li>
+              )}
+              {Product !== null && Product.attributes.storage && (
+                <li>
+                  <span className="label">Storage: </span>
+                  {Product.attributes.storage}
+                </li>
+              )}
+              {/*Accessories attributes */}
+              {Product !== null && Product.attributes.accessoriesBrand && (
+                <li>
+                  <span className="label">Brand: </span>{" "}
+                  {Product.attributes.accessoriesBrand}
+                </li>
+              )}
+              {Product !== null && Product.attributes.accessoriesColor && (
+                <li>
+                  <span className="label">Color: </span>
+                  {Product.attributes.accessoriesColor}
+                </li>
+              )}
+              {Product !== null && Product.attributes.accessoriesType && (
+                <li>
+                  <span className="label">Type: </span>
+                  {Product.attributes.accessoriesType}
+                </li>
+              )}
             </ul>
           </form>
         </div>
@@ -81,51 +307,3 @@ export default function SimpleSlider() {
     </>
   );
 }
-
-//fetch
-// import React, { useState, useEffect } from "react";
-// import Slider from "react-slick";
-// import Header from "../components/header.js";
-// import Navbar from "../components/navbar.js";
-// import Footer from "../components/footer.js";
-
-// export default function SimpleSlider() {
-//   const [productData, setProductData] = useState({});
-
-//   useEffect(() => {
-//     // Make an API call to fetch product data
-//     fetch("/api/products/6547ec8443469b90068ee70f")
-//       .then((response) => response.json())
-//       .then((data) => setProductData(data))
-//       .catch((error) => console.error(error));
-//   }, []);
-
-//   return (
-//     <>
-//       <Header />
-//       <Navbar />
-//       <div className="SingleProduct">
-//         <div className="SingleProduct_name">
-//           <span>{productData.name}</span>
-//         </div>
-//         <div className="carousel_container">
-//           <Slider {...settings}>
-//             <div>
-//               <img className="cimage" src={mouse} alt="mouse" />
-//             </div>
-//             <div>
-//               <img className="cimage" src={phone1} alt="logo" />
-//             </div>
-//             <div>
-//               <img className="cimage" src={phone} alt="phone" />
-//             </div>
-//           </Slider>
-//         </div>
-//         <div className="desc">
-//           <h3>$ {productData.price}</h3>
-//         </div>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// }
